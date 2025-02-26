7ef629ccee5f2e0aabe872c77fb29e81cf5b5974
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-02-26 16:36:44 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def index():
+    return render_template("index.html") 
+
+@socketio.on("message")
+def handle_message(msg):
+    print(f"Mensagem recebida: {msg}")
+    send(msg, broadcast=True)  
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
